87ccdd1a7ac7c8438f4f4568602130b67b9d3921
GoodGosha <80222546+GoodGosha@users.noreply.github.com>
2026-04-16 14:11:23 +0300

    Add files via upload

---
+++ voxnet_balance/custom_components/config_flow.py
@@ -0,0 +1,22 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from .const import DOMAIN
+
+class VoxnetConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+
+        if user_input:
+            return self.async_create_entry(
+                title="Voxnet",
+                data=user_input,
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("username"): str,
+                vol.Required("password"): str,
+            }),
